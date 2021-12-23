cb77fa3297b760f6ace4265408fb978afd5a5dd3
Vasiliy <kkolyasev1228@gmail.com>
2021-12-23 18:04:38 +0400

    Autoloder

---
+++ Autoloder/classes/Product.php
@@ -0,0 +1,45 @@
+<?php
+namespace classes;
+
+abstract class Product
+{
+    private $name;
+    protected $price;
+
+    private $discount = 0;
+
+    public function __construct($name, $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getProduct() {
+        return "<hr> <b>О товаре</b><br>
+                Наименование: {$this->name}<br>
+                Цена: {$this->getPrice()}<br>";
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getPrice()
+    {
+        return $this->price - ($this->discount/100 * $this->price);
+    }
+
+    public function getDiscount(): int
+    {
+        return $this->discount;
+    }
+
+    public function setDiscount(int $discount): void
+    {
+        $this->discount = $discount;
+    }
+
+    abstract protected function addProduct();
+
+}
---
+++ Autoloder/interfaces/I3D.php
@@ -0,0 +1,9 @@
+<?php
+namespace interfaces;
+
+interface I3D
+{
+    const TEST2 = 'test interface';
+
+    public function test();
+}
