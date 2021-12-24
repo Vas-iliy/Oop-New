7308c699e0e8048b0fd62130a58a205640c131e0
Vasiliy <kkolyasev1228@gmail.com>
2021-12-24 11:01:10 +0400

    Magic_Methods

---
+++ Magic_Methods/classes/BookProduct.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace classes;
+
+
+
+class BookProduct extends Product
+{
+    public $numPages;
+
+    public function __construct($name, $price, $numPages)
+    {
+        parent::__construct($name, $price);
+        $this->numPages = $numPages;
+        $this->setDiscount(5);
+    }
+
+    public function __toString()
+    {
+        return $this->getProduct();
+    }
+
+    public function __get($name)
+    {
+        return;
+    }
+
+    public function __set($name, $value)
+    {
+        return;
+    }
+
+    public function getProduct()
+    {
+        $out = parent::getProduct();
+        $out .= "Цена без скидки: {$this->price}<br>";
+        $out .= "Кол-во страниц {$this->numPages}<br>";
+
+        return $out;
+    }
+
+    public function getNumPages()
+    {
+        return $this->numPages;
+    }
+}
---
+++ Magic_Methods/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use classes\BookProduct;
+
+function autoloder($class) {
+    $class = str_replace("\\", '/', $class);
+    $file = __DIR__ . "/{$class}.php";
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+spl_autoload_register('autoloder');
+
+$book = new BookProduct('Gays', 2500, 500);
+$book->test = 'hello';
+echo $book;
